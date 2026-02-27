fb7ba886e5c3ddbfef74c8b8284d1692e0ce10af
uyronmarche14 <uyronmarcherhyssq@gmail.com>
2026-02-28 00:38:01 +0800

    chore: archive legacy code into .legacy/ directory (Phase 1)

--- src/utils/qmqa/index.js
+++
@@ -1,8 +0,0 @@
-/**
- * QMQA Utilities Index
- * Exports all QMQA utility functions
- */
-
-export * from './control-no-generator.js';
-export * from './status-mapper.js';
-export * from './response-formatter.js';
